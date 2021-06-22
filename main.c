22b86d0a7bc1835ed616243be9536d353cbc620f
Arthur Grillo <arthur.grillo@usp.br>
2021-06-21 23:23:31 -0300

    Criado as funções de inicialiazação dos pinos e tabuleiro

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
+void inicializarPinos(int pinos[]);
+void inicializarTabuleiro(int tabuleiro[]);
+
 int main(int argc, char *argv[]){
 int pinos[16];
 int tabuleiro[16];
+
+inicializarPinos(pinos);
+inicializarTabuleiro(tabuleiro);
 }
 //teste
 //ehescura ehbaixa ehquadrada  ehoca
 //   0        0        0         0
+
+void inicializarPinos(int pinos[]){
+	for(int i=0; i<16; i++){
+		pinos[i]=i;
+	}
+}
+
+void inicializarTabuleiro(int tabuleiro[]){
+	for(int i=0; i<16; i++){
+		tabuleiro[i]=-1;
+	}
+}
